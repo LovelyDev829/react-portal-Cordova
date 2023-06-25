f4b33022f7bfb4e7833802a3be12d4c237cfe73a
LovelyDev829 <lovelydeveloper829@gmail.com>
2023-06-25 13:11:27 +0900

    maybe...

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,5 +1,5 @@
 const baseUrl = process.env.NODE_ENV === "production" 
-? process.env.NEXT_PUBLIC_API_URL
+? 'https://cordovalearning.onrender.com'
 : 'http://localhost:3000';
 
 export default baseUrl;
