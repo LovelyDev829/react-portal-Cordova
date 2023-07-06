d75006bc54c967218a1ede405131b88e20c4ec78
LovelyDev829 <lovelydeveloper829@gmail.com>
2023-07-06 11:35:18 +0900

    test

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl = process.env.NODE_ENV === "production" 
-? 'https://next-test-xgvt.onrender.com'
-// ? ''
+// ? 'https://next-test-xgvt.onrender.com'
+? 'https://react-portal-cordova.vercel.app'
 : 'http://localhost:3000';
 
 export default baseUrl;
